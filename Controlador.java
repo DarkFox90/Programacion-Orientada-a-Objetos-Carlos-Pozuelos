afb8639ceb15dcfb15c68b5b57a63cf627443139
DarkFox90 <85356306+DarkFox90@users.noreply.github.com>
2025-08-31 01:49:37 -0600

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+
+//agregué un constructor para que pudiera funcionar mis funciones en Main
 public class Controlador {
+    private Gimnasio gimnasio;
+
+    public Controlador(Gimnasio gimnasio){
+        this.gimnasio = gimnasio;
+    }
+
+    //cambié el parametro de verMIembros a un array para que pudiera retornar el valor correcto
+    public ArrayList<Miembro> verMiembros(){
+        return gimnasio.getMiembrosActivos();
+    }
+
+    public ArrayList<Entrenador> verEntrenadores() {
+        return gimnasio.getEntrenadoresActivos();
+    }
+
     
 }
