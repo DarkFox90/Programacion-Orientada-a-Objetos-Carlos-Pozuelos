cfae1660109b16e12ab78b8224efb75a52569ddb
DarkFox90 <85356306+DarkFox90@users.noreply.github.com>
2025-08-31 01:50:57 -0600

    Update Gimnasio.java

--- Gimnasio.java
+++ Gimnasio.java
@@ -5,18 +5,34 @@ public class Gimnasio {
     private ArrayList<Entrenador> entrenadoresActivos;
     private ArrayList<Rutina> rutinas;
 
+    public Gimnasio() {
+        this.miembrosActivos = new ArrayList<>();
+        this.entrenadoresActivos = new ArrayList<>();
+        this.rutinas = new ArrayList<>();
+    }
+
     public void agregarMiembro(Miembro miembro){
         miembrosActivos.add(miembro);
     }
 
-    public void agregarMiembro(Entrenador entrenador){
-            entrenadoresActivos.add(entrenador);
-        }
+    public void agregarEntrenador(Entrenador entrenador){
+        entrenadoresActivos.add(entrenador);
+    }
 
-    public void agregarMiembro(Rutina rutina){
+    public void agregarRutina(Rutina rutina){
         rutinas.add(rutina);
     }
 
-   
+   public ArrayList<Miembro> getMiembrosActivos() {
+        return miembrosActivos;
+   }
+
+    public ArrayList<Entrenador> getEntrenadoresActivos() {
+        return entrenadoresActivos;
+    }
+
+    public ArrayList<Rutina> getRutinas() {
+        return rutinas;
+    }
 
 }
